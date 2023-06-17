2bc608ef68f57f5a446cee7d5f23f37e8c787249
Rhonal Gonzalez <rhonalgnzl@gmail.com>
2023-06-16 22:33:00 -0300

    fixed arithmetic shift and added composite assignment example

--- PracticaOperadores/PracticaOperadores/Programs/NumeroBinarioProgram.cs
+++ PracticaOperadores/PracticaOperadores/Programs/NumeroBinarioProgram.cs
@@ -15,11 +15,27 @@ namespace PracticaOperadores.Programs
             Console.WriteLine("\nNúmero en base 10: " + bin + "\n");
             Console.WriteLine(bin + " en base 2: ");
             Console.WriteLine(Convert.ToString(bin, toBase: 2).PadLeft(32, '0'));
-            Console.WriteLine("\nDesplazando el bit más significaivo hacia la izquierda: ");
-            Console.WriteLine(Convert.ToString(bin << 1, toBase: 2).PadLeft(32, '0'));
+            Console.WriteLine("\nDesplazando aritméticamente el bit más significaivo: ");
+            Console.WriteLine(Convert.ToString(bin >>> 1, toBase: 2).PadLeft(32, '0'));
             Console.WriteLine("\nAhora el resultado del desplazamiento en base 10: ");
-            Console.WriteLine(Convert.ToString(bin << 1, toBase: 10).PadLeft(2, '0'));
+            Console.WriteLine(Convert.ToString(bin >>> 1, toBase: 10).PadLeft(2, '0'));
             Console.ReadLine();
+
+            Console.WriteLine("\nAhora uso de los operadores de asignación compuesta:\n");
+            int? nulo = null;
+            int a = 0;
+            Console.WriteLine("Valor inicial de a: " + a);
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine("Interación " + (i+1));
+                if (i > 1 & i <= 2 || i > 3)
+                    a++;
+                a += nulo??i;
+                if (i == 3)
+                    nulo = a;
+                else
+                    nulo = null;
+                Console.WriteLine("Valor de a ahora: " + a);
+            }
         }
     }
 }
